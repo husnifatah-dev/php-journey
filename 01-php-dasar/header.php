ed9de59ae0db85969c6a33edfaa614f4512ccd9d
husnifatah-dev <mcbckm4@gmail.com>
2026-05-11 20:57:07 +0700

    fix typo di css

--- 01-php-dasar/header.php
+++ 01-php-dasar/header.php
@@ -1,7 +1,7 @@
 <?php 
-echo "<div style='backgrorud-color: black; color: white; padding: 10px;'>";
-echo "<h2>SYESTEM MANAJEMEN PABRIK</h2>";
-echo "<p> PT> Husni Fatah Jaya</p>";
+echo "<div style='background-color: black; color: white; padding: 10px;'>";
+echo "<h2>SYSTEM MANAJEMEN PABRIK</h2>";
+echo "<p> PT. Husni Fatah Jaya</p>";
 echo "</div><br>";
 
 ?>
