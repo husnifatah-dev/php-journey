7da3169bb40c353bc55600270f221906ad6d2ced
husnifatah-dev <mcbckm4@gmail.com>
2026-05-19 23:29:49 +0700

    feat: menampilkan data pekerja dari MariaDB menggunakan PDO

---
+++ 02-php-database-pdo/002-read-data.php
@@ -0,0 +1,47 @@
+<?php 
+require "001-koneksi.php";
+
+echo "<h2>Daftar Pekerja PT Husni Makmur</h2>";
+
+try {
+    // menyiapkan perintah SQL (prepared statement sangat di rekomendasikan di PDO)
+    $sql = "SELECT * FROM pekerja";
+    $stmt = $pdo->prepare($sql);
+
+    // eksekusi perintahnya
+    $stmt->execute();
+    
+    // ambil semua datanya
+    $data_pekerja = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Cek apakah datanya ada?
+    if (count($data_pekerja) > 0) {
+        echo "<table border='1' cellpadding='10' cellspacing='0'>";
+        echo "<tr style='background-color: black; color: white;'>
+                    <th>ID</th>
+                    <th>Nama Karyawan</th>
+                    <th>Shift</th>
+                    <th>Posisi</th>
+            </tr>";
+    
+
+    // membongkar isi array pakai foreach
+    foreach ($data_pekerja as $baris) {
+        echo "<tr>";
+        echo "<td>" . $baris['id'] . "</td>";
+        echo "<td>" . $baris['nama'] . "</td>";
+        echo "<td>" . $baris['shift'] . "</td>";
+        echo "<td>" . $baris['posisi'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+    } else {
+        echo "<i>Belum ada data pekerja di dalam database.</i>";
+    }
+} catch (PDOException $e) {
+    echo "<b>[ERROR]</b> Gagal mengambil data: " . $e->getMessage();
+}
+
+?>
