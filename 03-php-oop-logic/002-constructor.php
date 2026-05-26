275cd85cda51a00e565d64a9aeda25ade7d78c0d
husnifatah-dev <mcbckm4@gmail.com>
2026-05-26 23:44:14 +0700

    Mempelajari magic method __construct pada PHP OOP

---
+++ 03-php-oop-logic/002-constructor.php
@@ -0,0 +1,37 @@
+<?php 
+echo "===   CONSTRUCTOR (FUNGSI OTOMATIS)   ====<br><br>";
+
+class KaryawanPabrik {
+    public $nama;
+    public $divisi;
+    public $shift;
+
+    public function __construct($nama_input, $divisi_input, $shift_input) {
+        $this->nama = $nama_input;
+        $this->divisi = $divisi_input;
+        $this->shift = $shift_input;
+
+        echo "<i>[SISTEM] Data karyawan baru bernama <b>{$this->nama}</b> berhasil didaftarkan.</i><br>";
+    }
+
+    public function cetakIdCard() {
+        return "
+        <div style='border: 3px solid black; padding: 10px; width: 250px; margin-bottom: 10px; margin-bottom: 10px'>
+       <b>ID CARD PT HUSNI FATAH MAKMUR</b><hr>
+       Nama: {$this->nama} <br>
+       Divisi: {$this->divisi} <br>
+       Shift: {$this->shift} <br>
+        </div>";
+    }
+}
+
+
+$pekerja1 = new KaryawanPabrik("Husni Fatah", "Produksi", "Malam");
+$pekerja2 = new KaryawanPabrik("Budi Lupi", "Gudang", "Pagi");
+
+echo "<br><b>[MENCETAK ID CARD]</b><br>";
+echo $pekerja1->cetakIdCard();
+echo $pekerja2->cetakIdCard();
+
+
+?>
