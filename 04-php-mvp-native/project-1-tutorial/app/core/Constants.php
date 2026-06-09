a87d7e008499e448dabe13da999ffebc7c37c4bf
husnifatah-dev <mcbckm4@gmail.com>
2026-06-09 23:32:42 +0700

    Mempelajari PHP MVC bagian view

---
+++ 04-php-mvp-native/project-1-tutorial/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php 
+
+define('BASEURL', 'http://localhost/project1/public');
--- 04-php-mvp-native/project-1-tutorial/app/views/home/index.php
+++ 04-php-mvp-native/project-1-tutorial/app/views/home/index.php
@@ -1,1 +1,9 @@
-<h1>Selamat Datang di Halaman Web Saya</h1>
+<div class="container">
+    <div class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Selamat Datang di Web Saya!</h1>
+        <p class="col-md-8 fs-4">Ini adalah Jumbotron yang dibuat menggunakan Bootstrap 5 utility classes. Tetap keren tanpa perlu komponen jumbotron yang lama.</p>
+        <button class="btn btn-primary btn-lg" type="button">Pelajari Selengkapnya</button>
+    </div>
+</div>
+</div>
