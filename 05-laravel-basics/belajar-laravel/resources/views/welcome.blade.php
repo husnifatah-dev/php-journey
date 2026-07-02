0f05adca98580bdcbb803d3391fbf47b350725a5
husnifatah-dev <mcbckm4@gmail.com>
2026-07-03 01:26:34 +0700

    Instalasi awal framework Laravel dan modifikasi route dasar

---
+++ 05-laravel-basics/belajar-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pabrik Laravel</title>
+<head>
+<body>
+    <h1>Halo, Selamat Datang di Sistem Baru!</h1>    
+    <p>Ini adalah hasil modifikasi rute Laravel pertamaku. Bye-bye routing manual xixi</p>
+</body>
+</html>
