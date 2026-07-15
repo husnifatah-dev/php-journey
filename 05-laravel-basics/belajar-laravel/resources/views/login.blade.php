e3a651ed7f9f2317cdb008ec556ed94c84ec37b3
husnifatah-dev <mcbckm4@gmail.com>
2026-07-15 19:34:16 +0700

    feat: membuat sistem autentikasi manual (login & logout) dan memproteksi rute CRUD dengan middleware auth

---
+++ 05-laravel-basics/belajar-laravel/resources/views/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Sistem Pabrik</title>
+</head>
+<body style="font-family: sans-serif; text-align: center; margin-top: 10px;">
+
+    <h2>Gerbang Sistem Manajemen Pabrik</h2>
+
+    @if(session('error'))
+        <p style="color: red;">{{session('error') }}</p>
+    @endif
+
+    <form action="/login" method="post" style="display: inline-block; text-align: left; border: 1px solid black; padding: 20px;">
+        @csrf
+        <label>Email</label>
+        <input type="email" name="email" required> <br><br>
+
+        <label for="password">Password</label>
+        <input type="password" name="password" required> <br><br>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+
+    
+</body>
+</html>
--- 05-laravel-basics/belajar-laravel/routes/web.php
+++ 05-laravel-basics/belajar-laravel/routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate']);
 
+ROute::middleware('auth')->group(function () {
 
-Route::get('/pegawai', [PegawaiController::class, 'index']);
-Route::get('/pegawai/create', [PegawaiController::class, 'create']);
-Route::post('/pegawai', [PegawaiController::class, 'store']);
-Route::get('/pegawai/{id}/edit', [PegawaiController::class, 'edit']);
-Route::put('/pegawai/{id}', [PegawaiController::class, 'update']);
-Route::delete('/pegawai/{id}', [PegawaiController::class, 'destroy']);
+    Route::get('/pegawai', [PegawaiController::class, 'index']);
+    Route::get('/pegawai/create', [PegawaiController::class, 'create']);
+    Route::post('/pegawai', [PegawaiController::class, 'store']);
+    Route::get('/pegawai/{id}/edit', [PegawaiController::class, 'edit']);
+    Route::put('/pegawai/{id}', [PegawaiController::class, 'update']);
+    Route::delete('/pegawai/{id}', [PegawaiController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
