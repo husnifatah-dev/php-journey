9d2dafeb2fcde04846983114e2eb66b996402cc0
husnifatah-dev <mcbckm4@gmail.com>
2026-08-24 22:41:16 +0700

    feat: membuat endpoint POST untuk menyimpan data pegawai via REST API

--- 05-laravel-basics/belajar-laravel/app/Http/Controllers/Api/PegawaiApiController.php
+++ 05-laravel-basics/belajar-laravel/app/Http/Controllers/Api/PegawaiApiController.php
@@ -18,4 +18,27 @@ class PegawaiApiController extends Controller
             'data' => $pegawai
         ], 200);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'posisi' => 'required|string|max:255',
+            'shift' => 'required|in:Pagi,Siang,Malam',
+            'departemen_id' => 'required|exists:departemens,id',
+        ]);
+
+        $pegawai = Pegawai::create([
+            'nama' => $request->nama,
+            'posisi' => $request->posisi,
+            'shift' => $request->shift,
+            'departemen_id' => $request->departemen_id,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pegawai berhasil ditambahkan via APi',
+            'data' => $pegawai,
+        ], 201);
+    }
 }
--- 05-laravel-basics/belajar-laravel/routes/api.php
+++ 05-laravel-basics/belajar-laravel/routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/pegawai', [PegawaiApiController::class, 'index']);
+Route::post('/pegawai', [PegawaiApiController::class, 'store']);
