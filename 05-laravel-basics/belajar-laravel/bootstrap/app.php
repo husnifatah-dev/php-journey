25e4404a0b20f68be2e761fa1a5b42f8014c4832
husnifatah-dev <mcbckm4@gmail.com>
2026-08-29 20:58:45 +0700

    fix: override exception handling di bootstap/app.php untuk return JSON saat error 404 di endpoint API

--- 05-laravel-basics/belajar-laravel/bootstrap/app.php
+++ 05-laravel-basics/belajar-laravel/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,8 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data tidak ditemukan di database.'
+                ], 404);
+            }
+        });   
     })->create();
